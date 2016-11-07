f6c4f044759bd662965ef49778cef3b91be99b2a
marcin iwanczyk <iwanczyk.marcin@o2.pl>
2016-11-07 23:18:46 +0100

    zrobione cwiczenia 2 i 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 (function () {
 
+//Selektory zad 2
+
+$("div.grid.grid-12").addClass("divColor");
+$('nav li a[href*="http"]').addClass("navLink");
+$('input[type=radio]:not(:checked)').addClass("inputColor");
+$('input[type=checkbox]:not(:checked)').addClass("inputColor");
+$("#text p:nth-child(1)").addClass("emptyPColor");
+$('.pagination > p').addClass("paginationitem");
+
+//Hamburger menu
+
+$('#hamburgermenu').click(function(){
+  $(this).toggleClass('open');
+  $('.menu__list').slideToggle().toggleClass('showHiddenMenu');
+});
+
 })();

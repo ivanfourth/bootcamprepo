aea3192b4cc5ad023d99bfd984482f452e913014
marcin iwanczyk <iwanczyk.marcin@o2.pl>
2016-11-09 23:22:38 +0100

    zadania ostatnie zmiany

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 (function () {
 
-//Selektory zad 2
+//Selektory
 
 $("div.grid.grid-12").addClass("divColor");
 $('nav li a[href*="http"]').addClass("navLink");
@@ -15,5 +15,37 @@ $('#hamburgermenu').click(function(){
   $(this).toggleClass('open');
   $('.menu__list').slideToggle().toggleClass('showHiddenMenu');
 });
+
+//Dynamiczne dodawanie
+
+$("#dodajElement").on("click",function(){
+  var element = $("input[name=element]").val();
+  if( element == "")
+      {
+          alert("Pole input jest puste");
+      }else{
+          $("#dynamicsection > ul").append("<li>"+element+"</li>");
+          $("input[name=element]").val("");
+      }
+
+});
+
+//Ajax i JSON
+var button = $("#button");
+
+$("button").on("click",function(){
+      $.ajax({
+        url: "https://jsonplaceholder.typicode.com/users",
+     }).done(function( data ) {
+      console.log(data.length);
+        for(var i = 0; i<data.length; i++)
+      {
+        $(".menu__data ul").append("<li><b>Name:</b> "+data[i].name+" <b>Username:</b> "+data[i].username+" <b>Email:</b> "+data[i].email+" <b>Phone:</b> "+data[i].phone+"</li>");
+      }
+      });
+
+  });
+
+
 
 })();
